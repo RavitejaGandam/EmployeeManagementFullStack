9a3b6e67aca4e23209ae97cf808673057971ca3a
Raviteja Gandam <gandamraviteja@gmail.com>
2024-05-16 11:08:35 +0530

    first commit

---
+++ employee-management/src/test/java/com/grt/employeemanagement/EmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grt.employeemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
